2c452d7bcc9c997dabd623a70e4650c283971af3
Linda Z <l.zhong06@gmail.com>
2016-08-10 14:00:07 -0700

    Adding a form to Event Scene

--- app/scenes/createEventScene.js
+++ app/scenes/createEventScene.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
-import {Container, Content, Header, Title, Button, Icon} from 'native-base';
+import {Container, Content, Header, Title, Button, Icon, List, ListItem, Input, InputGroup} from 'native-base';
 import { Actions } from 'react-native-router-flux';
 
 class Scene extends React.Component {
@@ -14,6 +14,24 @@ class Scene extends React.Component {
                 <Title>Create Event</Title>
             </Header>
             <Content>
+
+                {/* Form for the meetup */}
+                <List>
+                    <ListItem>
+                        <InputGroup borderType="underline">
+                            <Input placeholder="Name of your meetup"/>
+                        </InputGroup>
+
+                        <InputGroup borderType="underline">
+                            <Input placeholder="Location"/>
+                        </InputGroup>
+
+                        <InputGroup borderType="underline">
+                            <Input placeholder="Details"/>
+                        </InputGroup>
+                    </ListItem>
+
+                </List>
                 <Text>
                     Placeholder for Create Events SCene Screen
                 </Text>
